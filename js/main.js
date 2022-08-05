a719635e4112d345162ca50e90517c2cf5568c8f
Md Arif <68365734+arifypp@users.noreply.github.com>
2022-08-05 22:35:28 +0600

    added projects and counter

--- js/main.js
+++ js/main.js
@@ -1,76 +1,114 @@
 (function($) {
-    "use strict";
+"use strict";
 
-    $('.slider-active').slick({
-        dots: false,
-        arrows: false,
-        infinite: false,
-        speed: 300,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1,
-              infinite: true,
-              dots: true
-            }
-          },
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          }
-        ]
-      });
+  $('.slider-active').slick({
+    dots: false,
+    arrows: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
 
-      
-      $('.service-slides').slick({
-        dots: false,
-        infinite: true,
-        autoplay: true,
-		    autoplaySpeed:3000,
-        arrows: true,
-        prevArrow: '.fa-angle-left',
-        nextArrow: '.fa-angle-right',
-        speed: 300,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 3,
-              slidesToScroll: 1,
-              infinite: true,
-              dots: true
-            }
-          },
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 1
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          }
-        ]
-      });
+
+  $('.service-slides').slick({
+    dots: false,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed:3000,
+    arrows: true,
+    prevArrow: '.fa-angle-left',
+    nextArrow: '.fa-angle-right',
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+
+  var grid = $('.grid').isotope({
+    // set itemSelector so .grid-sizer is not used in layout
+    itemSelector: '.grid-item',
+    percentPosition: true,
+    masonry: {
+      // use element for option
+      columnWidth: '.grid-item'
+    }
+  })
+
+  // filter items on button click
+  $('.project-menu').on( 'click', 'button', function() {
+    var filterValue = $(this).attr('data-filter');
+    grid.isotope({ filter: filterValue });
+  });
+
+  // for menu active class 
+  $('.project-menu button').on('click', function(event) {
+    $(this).siblings('.active').removeClass('active');
+    $(this).addClass('active');
+    event.preventDefault();
+  });
+
+  // Pop up image
+  $('.pop-img').magnificPopup({
+    gallery: {
+      enabled: true
+    },
+    type: 'image'
+    // other options
+  });
+
+  $('.counter').counterUp({
+    delay: 10,
+    time: 1000
+  });
+
 })(jQuery);
